3e69f8271983b22a2fb4f6be1df61900d79e24d5
이성현 <zzennam@naver.com>
2018-07-03 17:32:26 +0900

    Update Solution.java

--- 30 Days Of Code/Day 23- BST Level-Order Traversal/Solution.java
+++ 30 Days Of Code/Day 23- BST Level-Order Traversal/Solution.java
@@ -1,56 +1,60 @@
+package BtsOrdertraversal;
 
-#include <iostream>
-#include <cstddef>
-#include <queue>
-#include <string>
-#include <cstdlib>
+import java.util.*;
+import java.io.*;
 
-using namespace std;	
-class Node{
-    public:
-        int data;
-        Node *left,*right;
-        Node(int d){
-            data=d;
-            left=right=NULL;
-        }
-};
-class Solution{
-    public:
-  		Node* insert(Node* root, int data){
-            if(root==NULL){
-                return new Node(data);
-            }
-            else{
-                Node* cur;
-                if(data<=root->data){
-                    cur=insert(root->left,data);
-                    root->left=cur;
-                }
-                else{
-                   cur=insert(root->right,data);
-                   root->right=cur;
-                 }           
-           return root;
-           }
-        }
-  
-  	void levelOrder(Node * root){
-      //Write your code here
-  	
-  
+class Node {
+	Node left, right;
+	int data;
+
+	Node(int data) {
+		this.data = data;
+		left = right = null;
 	}
-  
-  };//End of Solution
-int main(){
-    Solution myTree;
-    Node* root=NULL;
-    int T,data;
-    cin>>T;
-    while(T-->0){
-        cin>>data;
-        root= myTree.insert(root,data);
-    }
-    myTree.levelOrder(root);
-    return 0;
+}
+
+class Solution {
+	static void levelOrder(Node root) {
+		// Write your code here
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+
+		while (queue.peek() != null) {
+			Node node = queue.remove();
+			System.out.println("" + node.data + " ");
+			if (node.left != null) {
+				queue.add(node.left);
+			}
+			if (node.right != null) {
+				queue.add(node.right);
+			}
+		}
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		Node root = null;
+		while (T-- > 0) {
+			int data = sc.nextInt();
+			root = insert(root, data);
+		}
+		levelOrder(root);
+	}
 }
