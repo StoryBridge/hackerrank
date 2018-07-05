21ff2756a619985756ce7b3175a2b5e7f376d1dd
이성현 <zzennam@naver.com>
2018-07-05 21:17:20 +0900

    Update Solution.java

--- 30 Days Of Code/Day 22- Binary Search Trees/Solution.java
+++ 30 Days Of Code/Day 22- Binary Search Trees/Solution.java
@@ -3,9 +3,10 @@ package BinarySearchTrees;
 import java.util.Scanner;
 
 class Node {
-	Node left, right;
+	Node left, right; //what is it?
 	int data;
 
+	// Constructor
 	Node(int data) {
 		this.data = data;
 		left = right = null;
@@ -28,17 +29,23 @@ public class Solution {
 	}
 
 	public static Node insert(Node root, int data) {
+		System.out.println("data : " + data);
 		if (root == null) {
+			// First create root node
 			return new Node(data);
 		} else {
 			Node cur;
 			if (data <= root.data) {
 				cur = insert(root.left, data);
 				root.left = cur;
+				System.out.println("left");
 			} else {
 				cur = insert(root.right, data);
 				root.right = cur;
+				System.out.println("right");
 			}
+			
+			System.out.println("cur.data: " + cur.data);
 			return root;
 		}
 	}
@@ -46,12 +53,16 @@ public class Solution {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
+
+		// The root is the first input data
 		Node root = null;
 		while (T-- > 0) {
 			int data = sc.nextInt();
 			root = insert(root, data);
 		}
-		int height = getHeight(root);
-		System.out.println(height);
+		
+		//System.out.println("root is: " + root.data);
+		// int height = getHeight(root);
+		// System.out.println(height);
 	}
 }
