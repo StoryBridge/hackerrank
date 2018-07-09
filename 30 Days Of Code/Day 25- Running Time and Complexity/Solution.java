bb0ab8707675cc3e83b891e3735d1e427ebf335e
이성현 <zzennam@naver.com>
2018-07-09 18:33:26 +0900

    Update Solution.java

--- 30 Days Of Code/Day 25- Running Time and Complexity/Solution.java
+++ 30 Days Of Code/Day 25- Running Time and Complexity/Solution.java
@@ -1,9 +1,20 @@
-import java.io.*;
-import java.util.*;
+package RunningTimeandComplexity;
+
+import java.util.Scanner;
 
 public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			int val = sc.nextInt();
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    }
+			for(int i=2; i<=val/i; i++){
+			    if(val%i==0) val=1;
+			}
+
+			System.out.println(val==1?"Not prime":"Prime");
+		}
+
+	}
 }
