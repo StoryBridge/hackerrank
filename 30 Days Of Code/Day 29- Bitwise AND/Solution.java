3868534443bf4f60cd0f0462bd1d6c409e644f69
이성현 <zzennam@naver.com>
2018-07-23 18:01:59 +0900

    Update Solution.java

--- 30 Days Of Code/Day 29- Bitwise AND/Solution.java
+++ 30 Days Of Code/Day 29- Bitwise AND/Solution.java
@@ -1,29 +1,54 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+package BitwiseAND;
+
+import java.util.Scanner;
 
 public class Solution {
+	private static final Scanner scanner = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		int t = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+		for (int tItr = 0; tItr < t; tItr++) {
+			String[] nk = scanner.nextLine().split(" ");
 
-    private static final Scanner scanner = new Scanner(System.in);
+			int n = Integer.parseInt(nk[0]);
 
-    public static void main(String[] args) {
-        int t = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+			int k = Integer.parseInt(nk[1]);
+			Find find = new Find(n, k);
+			find.possibleValue();
+			System.out.println(find.max());
+		}
 
-        for (int tItr = 0; tItr < t; tItr++) {
-            String[] nk = scanner.nextLine().split(" ");
+		scanner.close();
+	}
+}
 
-            int n = Integer.parseInt(nk[0]);
+class Find {
+	private int n;
+	private int k;
+	private int temp;
+	private int max = 0;
 
-            int k = Integer.parseInt(nk[1]);
-        }
+	Find(int n, int k) {
+		this.n = n;
+		this.k = k;
+	}
 
-        scanner.close();
-    }
+	public void possibleValue() {
+		for (int i = 1; i < n; i++) {
+			for (int j = i + 1; j <= n; j++) {
+				temp = i & j;
+				if (temp < k & temp > max) {
+					max = temp;
+				}
+			}
+
+		}
+	}
+
+	public int max() {
+		return max;
+	}
+
 }
