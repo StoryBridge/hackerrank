5a3b132829d80a315072b2987b65e3d055990ff3
이성현 <zzennam@naver.com>
2018-08-30 21:44:11 +0900

    Unsolved
    Think about case r is 1

--- Interview Preparation Kit/Dictionaries and Hashmaps/Count Triplets/Solution.java
+++ Interview Preparation Kit/Dictionaries and Hashmaps/Count Triplets/Solution.java
@@ -1,16 +1,12 @@
 package CountTriplets;
 
-import java.io.*;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
-
 	// Complete the countTriplets function below.
 	static long countTriplets(List<Long> arr, long r) {
 		// Key is value, value is counter
@@ -27,18 +23,27 @@ public class Solution {
 			}
 		}
 
-		// Hashmap sort by key
-		List<Long> hmList = new ArrayList<Long>(hm.keySet());
-		Collections.sort(hmList);
+		if (r == 1) {
+			// 1 1 1 1 1 1
+			//
+			// 3 -> 1
+			// 4 -> 3
+			// 5 -> 6
+			// 6 -> 10
+			//
 
-		for (int i = 0; i < hmList.size() - 2; i++) {
-			// check to constrains key
-			// System.out.println(hmList.get(i));
-			if (hmList.get(i) * r == hmList.get(i + 1) && hmList.get(i + 1) * r == hmList.get(i + 2)) {
-				count = count + hm.get(hmList.get(i)) * hm.get(hmList.get(i + 1)) * hm.get(hmList.get(i + 2));
+			return 0;
+		}
+
+		// System.out.println(hm.keySet());
+		// System.out.println(hm.values());
+		for (Long long1 : hm.keySet()) {
+			if (hm.containsKey(long1 * r) && hm.containsKey(long1 * r * r)) {
+				long plus = hm.get(long1) * hm.get(long1 * r) * hm.get(long1 * r * r);
+				count += plus;
 			}
 		}
-		System.out.println(hmList);
+
 		System.out.println(count);
 		return count;
 	}
